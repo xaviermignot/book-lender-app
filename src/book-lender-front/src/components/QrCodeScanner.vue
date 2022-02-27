c13bd2ef28d7f1f8bbd1597177d357a784485202
Xavier Mignot <xavier.mignot@floa.com>
2022-02-27 22:39:36 +0100

    feat: Make scan work in an old style way...

--- src/book-lender-front/src/components/QrCodeScanner.vue
+++ src/book-lender-front/src/components/QrCodeScanner.vue
@@ -1,32 +1,35 @@
-<script setup lang="ts">
+<script lang="ts">
 import { Html5QrcodeScanner } from 'html5-qrcode'
-import {onMounted} from 'vue'
 
-const props = defineProps({
-  qrbox: Number,
-  fps: Number
-})
-
-const emit = defineEmits<{
-  (e: 'result', decodedText: String, decodedResult: String): void
-}>()
-
-function onScanSuccess(decodedText: String, decodedResult: String) {
-  emit('result', decodedText, decodedResult);
+export default {
+  props: {
+    qrbox: {
+      type: Number,
+      default: 250
+    },
+    fps: {
+      type: Number,
+      default: 10
+    },
+  },
+  mounted() {
+    const config = {
+      fps: this.fps,
+      qrbox: this.qrbox,
+      experimentalFeatures: {
+        useBarCodeDetectorIfSupported: false
+      }
+    };
+    const html5QrcodeScanner = new Html5QrcodeScanner('qr-code-full-region', config, false);
+    html5QrcodeScanner.render(this.onScanSuccess);
+  },
+  methods: {
+    onScanSuccess(decodedText, decodedResult) {
+      this.$emit('result', decodedText, decodedResult);
+    }
+  }
 }
-
-onMounted(() => {
-  var config = {
-    fps: props.fps,
-    qrbox: props.qrbox
-  };
-
-  var html5QrcodeScanner = new Html5QrcodeScanner("qr-code-full-region", config, true);
-  html5QrcodeScanner.render(onScanSuccess, null);
-})
-
 </script>
-
 <template>
   <div id="qr-code-full-region"></div>
 </template>
--- src/book-lender-front/src/main.ts
+++ src/book-lender-front/src/main.ts
@@ -3,10 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import MenuLinkButton from './components/MenuLinkButton.vue'
-import QrCodeScanner from './components/QrCodeScanner.vue'
 
 createApp(App)
   .component('MenuLinkButton', MenuLinkButton)
-  .component('QrCodeScanner', QrCodeScanner)
   .use(router)
   .mount('#app')
