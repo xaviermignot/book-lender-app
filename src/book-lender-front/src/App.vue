dd27a8ccb7807a5315453e343441507515e0bbe5
Xavier Mignot <xavier.mignot@floa.com>
2022-03-07 22:53:47 +0100

    fix: Layout on mobile

--- src/book-lender-front/src/App.vue
+++ src/book-lender-front/src/App.vue
@@ -5,7 +5,7 @@ import { RouterView } from 'vue-router'
 <template>
   <main>
     <div class="container overflow-hidden">
-      <div class="d-grid gap-2 col-8 col-md-5 mx-auto mt-2 p-2 rounded bg-warning bg-opacity-25">
+      <div class="d-grid gap-2 col-md-5 mx-auto mt-2 p-2 rounded bg-warning bg-opacity-25">
         <RouterView />
       </div>
     </div>
