47c9ec43a1fb030891dcae31fb1bf7a542cfa674
Xavier Mignot <code@xmi.fr>
2022-05-02 23:53:12 +0200

    wip: Start using i18n

--- src/book-lender-front/src/main.ts
+++ src/book-lender-front/src/main.ts
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'vue-i18n'
+
 import MenuLinkButton from './components/MenuLinkButton.vue'
+
+const i18n = createI18n({
+  locale: 'fr'
+})
 
 createApp(App)
   .component('MenuLinkButton', MenuLinkButton)
   .use(router)
+  .use(i18n)
   .mount('#app')
--- src/book-lender-front/src/views/HomeView.vue
+++ src/book-lender-front/src/views/HomeView.vue
@@ -1,10 +1,30 @@
+<i18n>
+{
+  "en": {
+    "title": "Home page",
+    "lend": "Lend a book",
+    "list": "List the books"
+  },
+  "fr": {
+    "title": "Accueil",
+    "lend": "Prêter un livre",
+    "list": "Afficher la liste des livres"
+  } 
+}
+</i18n>
+
 <script setup lang="ts">
 import MenuLinkButton from '../components/MenuLinkButton.vue'
+import { useI18n } from 'vue-i18n';
+
+const { locale, t } = useI18n({
+  inheritLocale: true
+})
 </script>
 
 <template>
-  <h1>Home page</h1>
-  <MenuLinkButton to="/lend">Lend a book</MenuLinkButton>
-  <MenuLinkButton to="/return">Return a book</MenuLinkButton>
-  <MenuLinkButton to="/list">List the books</MenuLinkButton>
+  <h1>{{ t('title') }}</h1>
+  <MenuLinkButton to="/lend">{{ t('lend') }}</MenuLinkButton>
+  <MenuLinkButton to="/return">{{ t('lend') }}</MenuLinkButton>
+  <MenuLinkButton to="/list">{{ t('list') }}</MenuLinkButton>
 </template>
