2a3a2496d1417a76cf5b2f0062a6d33348c33a0c
Xavier Mignot <code@xmi.fr>
2025-03-04 09:01:11 +0100

    chore: Upgrade deps

--- src/book-lender-front/src/components/MenuLinkButton.vue
+++ src/book-lender-front/src/components/MenuLinkButton.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-const props = defineProps({
-  to: String
-})
+import { defineProps } from 'vue';
+import { RouteLocationRaw } from 'vue-router';
+
+const props = defineProps<{
+  to: string | RouteLocationRaw;
+}>();
 </script>
+
 <template>
   <router-link class="btn btn-success opacity-75" role="button" :to="to">
     <slot />
--- src/book-lender-front/vite.config.ts
+++ src/book-lender-front/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n()
+    VueI18nPlugin({
+      include: 'src/locales/**'
+    })
   ]
 })
